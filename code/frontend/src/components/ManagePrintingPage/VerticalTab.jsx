8f3cf8e984795936100f05a269a11be615aa30e3
PvD <pvdzxc@gmail.com>
2023-11-20 08:53:10 +0700

    FE: ManagePrintingPage

---
+++ code/frontend/src/components/ManagePrintingPage/VerticalTab.jsx
@@ -0,0 +1,89 @@
+import {
+    Tabs,
+    TabsHeader,
+    TabsBody,
+    Tab,
+    TabPanel,
+    Card,
+  } from "@material-tailwind/react";
+  import {
+    Cog6ToothIcon,
+    FlagIcon
+  } from "@heroicons/react/24/solid";
+  export function  VerticalTabs(item)  {
+     const data = item
+    // const data = [
+        // [
+        //     {
+        //         label: "Cài đặt tặng giấy",
+        //         value: "1",
+        //         desc: <Card>
+        //             </Card>,
+        //       },
+        //       {
+        //         label: "Quản lý in ấn",
+        //         value: "2",
+        //         desc: <Card>
+        //             </Card>,
+        //       },
+        //       {
+        //         label: "Xuất báo cáo định kỳ",
+        //         value: "3",
+        //         desc: <Card>
+        //             </Card>,
+        //       },
+        //  ];
+    //   {
+    //     label: "HTML",
+    //     value: "html",
+    //     desc: <Card>
+    //         </Card>,
+    //   },
+    // ];
+    const componentStyle = {
+        width: '200px',
+        height: '60px',
+        paddingLeft: '2%', paddingRight:'2%', paddingTop: '6%', paddingBottom: '6%',
+        textAlign: 'left'
+        //backgroundColor: 'lightblue', 
+      };
+    return (
+      <Tabs value="1" orientation="vertical" >
+        <TabsHeader className="bg-transparent"
+        indicatorProps={{
+          className: "bg-gray-900/10 shadow-none !text-gray-900",
+        }}
+        
+        >
+          {data.map(({ label, value, icon }) => (
+            <Tab key={value} value={value} className="place-items-start" 
+            style={componentStyle}
+                //{icon}
+            >
+              <div>
+                <text style={{textAlign: 'left'}}>
+                    {label}
+                </text>
+                
+              </div>
+            </Tab>
+          ))}
+          {/* {data.map(({ label, value, icon }) => (
+          <Tab key={value} value={value} className="place-items-start">
+            <div className="flex items-center gap-2">
+              {React.createElement(icon, { className: "w-5 h-5" })}
+              {label}
+            </div>
+          </Tab>
+            ))} */}
+        </TabsHeader>
+        <TabsBody style={{width : '60%', alignItems: 'flex-start'}}>
+          {data.map(({ value, desc }) => (
+            <TabPanel key={value} value={value} className="py-0">
+              {desc}
+            </TabPanel>
+          ))}
+        </TabsBody>
+      </Tabs>
+    );
+  }
--- code/frontend/src/pages/admin/ManagePrintingPage.jsx
+++ code/frontend/src/pages/admin/ManagePrintingPage.jsx
@@ -1,5 +1,75 @@
+import React from "react";
+import { VerticalTabs } from "../../components/ManagePrintingPage/VerticalTab";
+import {
+  Breadcrumbs,
+  Card,
+} from "@material-tailwind/react";
+import {
+  Cog6ToothIcon,
+  FlagIcon
+} from "@heroicons/react/24/solid";
 const ManagePrintingPage = () => {
-  return <div>ManagePrintingPage</div>;
+  let items = [
+    {
+      label: "Cài đặt tặng giấy",
+      value: "1",
+      //icon: React.createElement(Cog6ToothIcon, { className: "w-5 h-5" }),
+      desc: <Card>
+        123123123
+          </Card>,
+    },
+    {
+      label: "Cài đặt kiểu file in",
+      value: "2",
+      icon: FlagIcon,
+      desc:
+        //ACCESS DATABASE, GET TYPE LIST, EXPORT TO BELOW COMPONENT 
+        <Card>
+          ĐÂY LÀ TYPE LIST
+        </Card>,
+    },
+    {
+      label: "Xuất báo cáo định kỳ",
+      value: "3",
+      desc: <Card>
+        7897898789
+          </Card>,
+    },
+  ];
+  const componentStyle = {
+    width: '200px',
+    height: '50px',
+    outerHeight: '50px',
+    textAlign: 'left'
+    //backgroundColor: 'lightblue', 
+  };
+  let aaa = VerticalTabs(items)
+  return (
+    <Card 
+    style={{
+      width : '100%',
+      paddingLeft: '4%', paddingRight:'4%', paddingTop: '6%', paddingBottom: '6%',
+      }}>
+      
+      <text style={{fontWeight : 'bold', fontSize : '30px'}}>
+        QUẢN LÝ IN ẤN
+      </text>
+      <Breadcrumbs>
+        <a href="../" className="opacity-60">
+          Trang chủ
+        </a>
+        <a href="#" className="opacity-60">
+          Quản lý in ấn
+        </a>
+        <a href="#">Cài đặt kiểu file in</a>
+      </Breadcrumbs>
+      {/* <text style={componentStyle}>
+        Menu
+      </text> */}
+      {aaa}
+
+    </Card>
+  )
 };
 
 export default ManagePrintingPage;
