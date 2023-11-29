f59697baf6d8dee42a3818c75029ac832a209be4
PvD <pvdzxc@gmail.com>
2023-11-29 21:32:15 +0700

    add: perfect icon handle

--- code/frontend/src/pages/admin/ManagePrintingPage.jsx
+++ code/frontend/src/pages/admin/ManagePrintingPage.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { VerticalTabs } from "../../components/ManagePrintingPage/VerticalTab";
+import { VerticalTabs } from "../../components/ManagePrintingPage/VerticalTab.jsx";
+import ButtonList from "../../components/ManagePrintingPage/ButtonList.jsx";
 //import { CookieDialog} from "../../components/ManagePrintingPage/Dialog";
 import docx from "../../assets/filetypeicon/docx.png"
 import jpg from "../../assets/filetypeicon/jpg.png"
 import pdf from "../../assets/filetypeicon/pdf.png"
 import svg from "../../assets/filetypeicon/svg.png"
 import xls from "../../assets/filetypeicon/xls.png"
-import plussign from "../../assets/filetypeicon/plus.png"
 import {
   Breadcrumbs,
   Button,
@@ -22,7 +22,7 @@ import {
   MapIcon,
   FlagIcon
 } from "@heroicons/react/24/solid";
-import CookieDialog from "../../components/ManagePrintingPage/Dialog";
+//  import CookieDialog from "../../components/ManagePrintingPage/Dialog";
 
 // const fs = require('fs');
 // const path = require('path');
@@ -79,7 +79,7 @@ const ManagePrintingPage = () => {
           </ButtonGroup>
         </Card>,
     },
-    {
+    { 
       label: "Xuất báo cáo định kỳ",
       value: "3",
       desc: <Card>
@@ -88,37 +88,53 @@ const ManagePrintingPage = () => {
     },
     */
   ];
-  let _typeList = ['pdf', 'jpg', 'docx', 'xls', 'svg']
-  let _pngList = [pdf, jpg, docx, xls, svg]
-  let _buttonList = []
-  for (let type in _typeList) {
-    _buttonList.push(
-      // <Button className="w-30 bg-red-700 m-4">
-      //   <img className="" src={_pngList[type]} alt={_typeList[type]} />
-      // </Button>
-      <CookieDialog 
-        img = {<img className="" src={_pngList[type]} alt={_typeList[type]} />}
-        text = {<text>Bạn muốn xóa định dạng {_typeList[type]}</text>}
-        
-      />
-      )
+  let _toggle = [1,2,3,4,5]
+  let _typeList = ['pdf', 'jpg', 'docx', 'xls', 'svg', 'xlsx', 'pptx', 'png', 'xps',]
+  let _pngList = []
+  for (let type in _typeList){
+    _pngList.push("/src/assets/filetypeicon/" + _typeList[type] + '.png');  
   }
-  _buttonList.push(
-    // <Button className="w-30 bg-red-700 m-4">
-    //   <img className="" src={_pngList[type]} alt={_typeList[type]} />
-    // </Button>
-    <CookieDialog 
-      img = {<img className="" src={plussign} alt={'add'} />}
-      text = {<text>Bạn muốn thêm định dạng nào?</text>}
+  let _buttonList = []
+  const Vertab = () => {
+
+    const handleButtonClick = (idx) => {
+      // Update the state to trigger a re-render
+      _toggle[idx] = 1 - _toggle[idx]
+    };
+  
+
+  // for (let type in _typeList) {
+  //   if (_toggle[type] === 1)
+  //   _buttonList.push(
+  //     <Button className="w-30 bg-red-700 m-4">
+  //       <img className="" src={_pngList[type]} alt={_typeList[type]} />
+  //     </Button>
+  //     // {
+  //     //   idx: type,
+  //     //   comp: <CookieDialog onClick={handleButtonClick(type)}
+          
+  //     //   img = {<img className="" src={_pngList[type]} alt={_typeList[type]} />}
+  //     //   text = {<text>Bạn muốn xóa định dạng {_typeList[type]}</text>}
+  //     //   />
+  //     // }
+  //     )
+  // }
+  // _buttonList.push(
+  //   // <Button className="w-30 bg-red-700 m-4">
+  //   //   <img className="" src={_pngList[type]} alt={_typeList[type]} />
+  //   // </Button>
+  //   <CookieDialog 
+  //     img = {<img className="" src={plussign} alt={'add'} />}
+  //     text = {<text>Bạn muốn thêm định dạng nào?</text>}
       
-    />
-    )
+  //   />
+  //   )
   let newtabItems = createTabItem(
-    "Cài đặt loại file in",
-    1,
+    "Cài đặt loại file in", 1,
     React.createElement(MapIcon, { className: "w-5 h-5" }),
-    <div class="grid grid-cols-2 gap-8 lg:grid-cols-4 bg-blue-200">
-      {_buttonList}
+      //{_buttonList.map(_buttonList => _buttonList.comp)}
+    <div>
+      {ButtonList(_toggle,_typeList,_pngList)}
     </div>)
   items.push(newtabItems)
   newtabItems = createTabItem("Cài đặt tặng giấy", 2,
@@ -126,7 +142,7 @@ const ManagePrintingPage = () => {
     <Card>
     </Card>)
   items.push(newtabItems)
-  newtabItems = createTabItem("Cài đặt tặng giấy", 3,
+  newtabItems = createTabItem("Xuất báo cáo", 3,
   React.createElement(Cog8ToothIcon, { className: "w-5 h-5" }),
     <Card>
     </Card>)
@@ -138,15 +154,20 @@ const ManagePrintingPage = () => {
     textAlign: 'left'
     //backgroundColor: 'lightblue', 
   };
-  let VerticalTab = VerticalTabs(items)
+  return VerticalTabs(items)
+  //let VerticalTab = VerticalTabs(items)
+  } 
+  
   return (
+    
     <div className="grid grid-rows-12 grids-cols-12 gap-5 m-5">
       <div className="flex items-center h-20">
+        <div className="cols-span-5 items-center h-20">
         <Typography variant="h3" className="basis-1/5 font-bold" >
           QUẢN LÝ IN ẤN
         </Typography>
-
-        <div className=" basis-4/5">
+        </div>
+        <div className="cols-span-7 items-center">
           <Breadcrumbs>
             <a href="../" className="opacity-60">
               Trang chủ
@@ -161,7 +182,7 @@ const ManagePrintingPage = () => {
       {/* <text style={componentStyle}>
           Menu
         </text> */}
-      {VerticalTab}
+      {Vertab()}
     </div>
   )
 };
